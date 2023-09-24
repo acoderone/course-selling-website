90666552f3aa494f4ccb9e810e26a5f301d88d34
acoderone <41126397+acoderone@users.noreply.github.com>
2023-09-24 18:07:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const cors = require('cors');
 const mongoose=require('mongoose'); //help to connect to mongodb
 require('dotenv').config();
 app.use(express.json());
-app.use(cors(
-  {
-    origin:["https://course-selling-website-alpha.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
+app.use(
+  cors({
+    origin: ['https://course-selling-website-alpha.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true,
+  })
+);
 
 const PORT=process.env.PORT || 8000;
 let ADMINS = [];
